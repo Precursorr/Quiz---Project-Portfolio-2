c2c6c58448ca5acaa322ab5c3d39248ccffeb01f
Denis Berrill <denisberrill98@gmail.com>
2024-06-24 15:07:48 +0000

    Began quiz function

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,13 +17,34 @@ window.onclick = function(event) {
 
 // HTML & CSS History popup
 
-let history = document.getElementById("historyInfo");
+const history = document.getElementById("historyInfo");
 
 function btnHistory(){
   history.style.display = "block";
 }
 
-function btnReturn(){
+function btnHistoryReturn(){
   history.style.display = "none"
 }
 
+// Begin quiz 1 with first question and answers selection
+let quiz1 = document.getElementById("quiz1")
+let btnBegin = document.getElementById("begin")
+let question = document.getElementById("question")
+let answersA = document.getElementById("answers1")
+let answersB = document.getElementById("answers2")
+let answersC = document.getElementById("answers3")
+let answersD = document.getElementById("answers4")
+
+function begin(){
+  history.style.display = "none";
+  quiz1.style.display = "block";
+  btnBegin.style.display = "none";
+  question.innerHTML="Which character is used to indicate an end tag?";
+  answersA.innerHTML="/";
+  answersB.innerHTML="*";
+  answersC.innerHTML="<";
+  answersD.innerHTML=">";
+}
+
+// When first question is answered, second question and answers selection will appear
