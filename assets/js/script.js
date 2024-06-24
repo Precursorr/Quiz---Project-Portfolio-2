7c7f700ec7ab0784a5ee729d0cd031a109ca02df
Denis Berrill <denisberrill98@gmail.com>
2024-06-24 16:21:32 +0000

    Begin function now didsplays question and answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,24 +27,37 @@ function btnHistoryReturn(){
   history.style.display = "none"
 }
 
-// Begin quiz 1 with first question and answers selection
-let quiz1 = document.getElementById("quiz1")
-let btnBegin = document.getElementById("begin")
-let question = document.getElementById("question")
-let answersA = document.getElementById("answers1")
-let answersB = document.getElementById("answers2")
-let answersC = document.getElementById("answers3")
-let answersD = document.getElementById("answers4")
+// Begin quiz 1 with first options
+let answers = Array.from(document.getElementsByClassName("answers"))
+let questions1 = {
+  question: "Which character is used to indicate an end tag?",
+  answer: 1,
+  answers: ["/", "*", "<", ">"]
+}
+
+let questions2 = {
+  question: "Which of these elements are all <table> elements?",
+  answer: 1,
+  answers: ["<table><tr><td>"]
+}
 
 function begin(){
   history.style.display = "none";
   quiz1.style.display = "block";
   btnBegin.style.display = "none";
-  question.innerHTML="Which character is used to indicate an end tag?";
-  answersA.innerHTML="/";
-  answersB.innerHTML="*";
-  answersC.innerHTML="<";
-  answersD.innerHTML=">";
+  document.getElementById("question").innerHTML = questions1.question
+  document.getElementById("answers1").innerHTML = questions1.answers[0]
+  document.getElementById("answers2").innerHTML = questions1.answers[1]
+  document.getElementById("answers3").innerHTML = questions1.answers[2]
+  document.getElementById("answers4").innerHTML = questions1.answers[3]
 }
 
-// When first question is answered, second question and answers selection will appear
+function answer(){
+  if (userAnswer === questions.answers[0]) {
+    document.getElementById("question").innerHTML = questions2.question
+    document.getElementById("answers1").innerHTML = questions2.answers[0]
+    document.getElementById("answers2").innerHTML = questions2.answers[1]
+    document.getElementById("answers3").innerHTML = questions2.answers[2]
+    document.getElementById("answers4").innerHTML = questions2.answers[3]
+  }
+}
