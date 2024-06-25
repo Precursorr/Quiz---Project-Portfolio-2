b5398630bb49d40e8babe26d48725215100c9e3a
Denis Berrill <denisberrill98@gmail.com>
2024-06-25 16:16:04 +0000

    Add media query for 55px screens and smaller

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,33 +27,32 @@ function btnHistoryReturn(){
   history.style.display = "none"
 }
 
-// Begin quiz 1 with first options
-let answers = Array.from(document.getElementsByClassName("answers"))
+// Quiz Array - HTML & CSS
 let questions = [{
   question: "Which character is used to indicate an end tag?",
-  answer: 1,
   answers: ["/", "*", "<", ">"]
 },
 { question: "Which of these elements are all &lt;table&gt; elements?",
-  answer: 1,
   answers: ["&lt;table&gt;&lt;tr&gt;&lt;td&gt;", "&lt;table&gt;&lt;tr&gt;&lt;tt&gt;", "&lt;thread&gt;&lt;body&gt;&lt;tr&gt;", "&lt;table&gt;&lt;head&gt;&lt;tfoot&gt;"]
-}]
+},
+{
+  question: "Which HTML attribute specifies an alternate text for an image, if the image cannot be displayed?",
+  answers: ["src", "longdesc", "alt", "title"]
+}];
 
-function begin(){
-  history.style.display = "none";
-  quiz1.style.display = "block";
+
+
+// When begin is pressed, the first quiz iteration pops up
+let btnBegin = document.getElementById("btnBegin")
+let quiz = document.getElementById("quiz1")
+
+function begin() {
+  history.style.display =  "none";
   btnBegin.style.display = "none";
-  document.getElementById("question").innerHTML = questions[0].question
-  document.getElementById("answers1").innerHTML = questions[0].answers[0]
-  document.getElementById("answers2").innerHTML = questions[0].answers[1]
-  document.getElementById("answers3").innerHTML = questions[0].answers[2]
-  document.getElementById("answers4").innerHTML = questions[0].answers[3]
+  quiz.style.display = "block"
+  document.getElementById("question").innerHTML = questions[0].question;
+  document.getElementById("answers1").innerHTML = questions[0].answers[0];
+  document.getElementById("answers2").innerHTML = questions[0].answers[1];
+  document.getElementById("answers3").innerHTML = questions[0].answers[2];
+  document.getElementById("answers4").innerHTML = questions[0].answers[3];
 }
-
-function answer(){
-    document.getElementById("question").innerHTML = questions[1].question
-    document.getElementById("answers1").innerHTML = questions[1].answers[0]
-    document.getElementById("answers2").innerHTML = questions[1].answers[1]
-    document.getElementById("answers3").innerHTML = questions[1].answers[2]
-    document.getElementById("answers4").innerHTML = questions[1].answers[3]
-  }
