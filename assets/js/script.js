1a95379d4319816a7fda27d08ed3745b011f761a
Denis Berrill <denisberrill98@gmail.com>
2024-06-25 19:42:50 +0000

    Add rest of questions and options in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,6 +38,34 @@ let questions = [{
 {
   question: "Which HTML attribute specifies an alternate text for an image, if the image cannot be displayed?",
   answers: ["src", "longdesc", "alt", "title"]
+},
+{
+  question: "The HTML global attribute, 'contenteditable' is used to:",
+  answers: ["Update content from server", "Return the position of the first found occurrence of content inside a string", "Specifies a context menu for an element. The menu appears when a user right-clicks on an element", "Specify whether the content of an element should be editable or not"]
+},
+{
+  question: "The HTML &lt;canvas&gt; element is used to:",
+  answers: [" manipulate data in MySQL", " create draggable elements", " draw graphics", " display database records"]
+},
+{
+  question: "What does CSS stand for?",
+  answers: [" Computer Style Sheets", " Colorful Style Sheets", " Cascading Style Sheets", " Creative Style Sheets"]
+},
+{
+  question: "Which HTML attribute is used to define inline styles?",
+  answers: ["Front", "Style", "Styles", "Class"]
+},
+{
+  question: "Which CSS property controls the text size?",
+  answers: ["font-size", "text-size", "text-style", "font-style"]
+},
+{
+  question: "What is the correct CSS syntax for making all the <p> elements bold?",
+  answers: ["p {font-weight:bold;}", "&lt;p style=&quot;text-size:bold;&quot;&gt;", "&lt;p style=&quot;font-size:bold;&quot;&gt;", "p {text-size:bold;}"]
+},
+{
+  question: "What is the default value of the position property?",
+  answers: ["absolute", "relative", "static", "fixed"]
 }];
 
 
